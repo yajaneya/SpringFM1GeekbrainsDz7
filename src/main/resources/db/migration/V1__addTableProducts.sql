590a49a81beb0574e1400e292b2fe4a6ec4e797a
yajaneya <janeya@yandex.ru>
2021-12-02 17:42:30 +0300

    1. Создать сущность «Товар» (id, название, стоимость) и соответствующую таблицу в БД. Заполнить таблицу тестовыми данными (20 записей).

---
+++ src/main/resources/db/migration/V1__addTableProducts.sql
@@ -0,0 +1,24 @@
+create table if not exists products (id bigserial primary key, name varchar(255), price int);
+
+insert into products (name, price)
+values
+('Milk', 50),
+('Bred', 30),
+('Solt', 15),
+('Soda', 12),
+('Tea', 75),
+('Apple', 55),
+('Banana', 75),
+('Orange', 100),
+('Potato', 60),
+('Cake', 95),
+('Cream', 81),
+('Salmon', 950),
+('Beef', 450),
+('Chiken', 180),
+('Hamburger', 115),
+('Pizza', 355),
+('Grape', 105),
+('Cheese', 515),
+('Butter', 650),
+('Steak', 555);

12813fb7a6221933025bde05f37a961ebdb57f9d
yajaneya <janeya@yandex.ru>
2021-12-02 16:42:37 +0300

    Initial commit

---
+++ src/main/java/ru/yajaneya/SpringFM1GeekbrainsDz7/SpringFm1GeekbrainsDz7Application.java
@@ -0,0 +1,13 @@
+package ru.yajaneya.SpringFM1GeekbrainsDz7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFm1GeekbrainsDz7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFm1GeekbrainsDz7Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/yajaneya/SpringFM1GeekbrainsDz7/SpringFm1GeekbrainsDz7ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yajaneya.SpringFM1GeekbrainsDz7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFm1GeekbrainsDz7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
